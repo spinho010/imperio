ce5b55d03f261b82e93894e913d61a9e18179542
LucasOliver <78925489+spinho010@users.noreply.github.com>
2021-05-31 17:05:43 -0300

    Update settings.py

--- imperio/imperionet/settings.py
+++ imperio/imperionet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$k!+l7nto#*x3^2i3%^lqd=kpq0mdix(o6+*3$7s^=x+*ipwm7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
